70ed5b18b1d6279fcf326701072b30f9b49ea6c9
BBudiharjo <BudimanaBudiharjo@gmail.com>
2025-03-21 12:38:57 -0700

    implemented part B and changed multiple implementations

---
+++ src/LA1/Model/AutoPlaylist.java
@@ -0,0 +1,40 @@
+package LA1.Model;
+
+import java.util.ArrayList;
+
+
+public class AutoPlaylist {
+	private String name;
+	private ArrayList<Song> songs;
+	
+	public AutoPlaylist(String name){
+		this.name = name;
+		this.songs = new ArrayList<Song>();
+	}
+	
+	public void addSongs(Song song){
+		this.songs.add(song);
+	}
+	
+	public void wipe(){
+		this.songs.clear();
+	}
+	
+	public void removeSong(Song song){
+		this.songs.remove(song);
+	}
+	
+	public int getPlaylistSize(){
+		return this.songs.size();
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public ArrayList<Song> getSongs() {
+	    return new ArrayList<>(songs);
+	}
+	
+}
+
--- src/LA1/Model/Song.java
+++ src/LA1/Model/Song.java
@@ -4,14 +4,11 @@ public class Song {
 	private String name;
 	private String artist;
 	private String album;
-	private int userRating;
-	private boolean favorite;
 	
 	public Song(String song, String artist, String album){
 		this.name = song;
 		this.artist = artist;
 		this.album = album;
-		this.favorite = false;
 	}
 	
 	public String getSongTitle(){
@@ -25,27 +22,4 @@ public class Song {
 	public String getAlbumTitle(){
 		return this.album;
 	}
-	// sets the rating between 1 and 5, and if rating is 5 auto set to favorite
-	public void setRating(int rating) {
-		if (rating >= 1 && rating <= 5) {
-			this.userRating = rating;
-			if (rating == 5) {
-				this.favorite = true;
-			}
-		}
-	}
-	 
-	public int getRating(){
-		return this.userRating;
-	}
-	
-	public boolean isFavorite() {
-		return this.favorite;
-	}
-	
-	public void setFavorite(boolean favorite) {
-        this.favorite = favorite;
-    }
-	
-	
 }
